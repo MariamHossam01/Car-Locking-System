cb57b95812f06e647da4aa8bf0eef15fabfc3e4a
MariamHossam01 <mariam.mostafa01@eng-st.cu.edu.eg>
2023-06-15 18:33:44 +0300

    GPT

--- Embedded_Final_Project/GPT/GPT_private.h
+++ Embedded_Final_Project/GPT/GPT_private.h
@@ -1,41 +1,46 @@
+/*
+ * GPT_Private.h
+ *
+ *  Created on: May 9, 2023
+ *      Author: hp
+ */
+
 #ifndef GPT_PRIVATE_H_
 #define GPT_PRIVATE_H_
 
-
 #include "Std_Types.h"
 
-#define TIM2 ((GPT_Type *)0x40000C00)
-#define TIM3 ((GPT_Type *)0x40000800)
-#define TIM4 ((GPT_Type *)0x40000400)
-#define TIM5 ((GPT_Type *)0x40000000)
-#define NVIC ((NVICType *)0xE000E100 )
+
+
+#define TIM5 ((GPT_Type *)0x40000C00)
+#define TIM4 ((GPT_Type *)0x40000800)
+#define TIM3 ((GPT_Type *)0x40000400)
+#define TIM2 ((GPT_Type *)0x40000000UL)
 
 typedef struct {
-	uint32 CR1;
-	uint32 CR2;
-	uint32 SMCR;
-	uint32 DIER;
-	uint32 SR;
-	uint32 EGR;
-	uint64 CCMR1;
-	uint64 CCMR2;
-	uint32 CCER;
-	uint32 CNT;
-	uint32 PSC;
-	uint32 ARR;
-	uint32 RES;
-	uint32 CCR1;
-	uint32 CCR2;
-	uint32 CCR3;
-	uint32 CCR4;
-	uint32 RES_1;
-	uint32 DCR;
-	uint32 DMAR;
-	uint32 TIM2_OR;
-	uint32 TIM5_OR;
-
+  uint32 CR1;
+  uint32 CR2;
+  uint32 SMCR;
+  uint32 DIER;
+  uint32 SR;
+  uint32 EGR;
+  uint32 CCMR1;
+  uint32 CCMR2;
+  uint32 CCER;
+  uint32 CNT;
+  uint32 PSC;
+  uint32 ARR;
+  uint32 RCR;
+  uint32 CCR1;
+  uint32 CCR2;
+  uint32 CCR3;
+  uint32 CCR4;
+  uint32 BDTR;
+  uint32 DCR;
+  uint32 DMAR;
 } GPT_Type;
 
+#define NVIC ((NVICType *)0xE000E100)
 typedef struct {
 	uint32 ISER0;
 	uint32 ISER1;
@@ -74,8 +79,5 @@ typedef struct {
 	uint32 IPR19;
 	uint32 IPR20;
 } NVICType;
-
-
-
 
 #endif /* GPT_PRIVATE_H_ */
