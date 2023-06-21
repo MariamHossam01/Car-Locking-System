6f75499c598bb05632cc4b7c00913177b8676fd9
aishawaziry <81515483+aishawaziry@users.noreply.github.com>
2023-06-21 18:12:17 +0300

    Update Gpio.h

--- Embedded_Final_Project/Gpio/Gpio.h
+++ Embedded_Final_Project/Gpio/Gpio.h
@@ -8,13 +8,20 @@
 #ifndef GPIO_H
 #define GPIO_H
 #include "Std_Types.h"
+#include "Bit_Operations.h"
+
+/*if we uses the second initial */
+#define GPIO_A_ARR_INDEX	0
+#define GPIO_B_ARR_INDEX	1
+#define GPIO_C_ARR_INDEX	2
+#define GPIO_D_ARR_INDEX	3
+
 
 #define GPIO_A 'A'
 #define GPIO_B 'B'
-#define GPIO_B 'C'
-#define GPIO_B 'D'
-#define GPIO_B 'E'
-#define GPIO_B 'H'
+#define GPIO_C 'C'
+#define GPIO_D 'D'
+
 
 #define GPIO_INPUT  0x00
 #define GPIO_OUTPUT 0x01
@@ -26,16 +33,22 @@
 #define GPIO_OPEN_DRAIN 0x01
 
 
-#define LOW    0x00   
+#define LOW    0x00
 #define HIGH   0x01
 
+/*DefaultState Input*/
+#define GPIO_NPUSH_NPULL  0x00
+#define GPIO_INPUT_PULL_UP 0x01
+#define GPIO_INPUT_PULL_DOWN 0x02
 
 #define GPIO_NO_PULL   (0x00 << 1)
 #define GPIO_PULL_UP   (0x01 << 1)
 #define GPIO_PULL_DOWN (0x02 << 1)
 
 void Gpio_ConfigPin(uint8 PortName, uint8 PinNum, uint8 PinMode, uint8 DefaultState);
-void Gpio_WritePin(uint8 PortName, uint8 PinNum, uint8 Data);
-uint8 Gpio_ReadPin(uint8 PortName, uint8 PinNum);
+void GPIO_Init(void);
+void GPIO_Init2(void);
+void Gpio_WritePin(uint8 PinId, uint8 PinData);
+uint8 Gpio_ReadPin(uint8 PinId) ;
 
 #endif /* GPIO_H */
